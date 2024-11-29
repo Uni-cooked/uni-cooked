501e85a069b08e208d502e563c391208c1cc3578
Prek <91291702+ErPreco@users.noreply.github.com>
2024-11-29 11:01:07 +0100

    Update create-table.sql

--- SQL/create-table.sql
+++ SQL/create-table.sql
@@ -11,7 +11,6 @@ DROP TABLE IF EXISTS Take_away CASCADE;
 DROP TABLE IF EXISTS Preferenza_take_away CASCADE;
 
 DROP DOMAIN IF EXISTS POSITIVE_SMALLINT;
-DROP DOMAIN IF EXISTS POSITIVE_TINYINT;
 
 DROP TYPE IF EXISTS VISIBILITA;
 DROP TYPE IF EXISTS CATEGORIA;
@@ -19,7 +18,6 @@ DROP TYPE IF EXISTS TIPO_PIATTO;
 DROP TYPE IF EXISTS UNITA_MISURA;
 
 CREATE DOMAIN POSITIVE_SMALLINT AS SMALLINT CHECK (VALUE>=0);   -- 2 Byte
-CREATE DOMAIN POSITIVE_TINYINT AS TINYINT CHECK (VALUE>=0);   -- 1 Byte
 
 CREATE TYPE VISIBILITA AS enum ('privata', 'pubblica');
 CREATE TYPE CATEGORIA AS enum ('fuorisede', 'in_sede', 'pendolare', 'dad', 'take_away');
@@ -30,13 +28,13 @@ CREATE TABLE Utente(
     email VARCHAR(100) PRIMARY KEY,
     nome VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(64) NOT NULL,  -- SHA256 restituisce sempre una stringa di 64 caratteri
-    data_iscrizione DATE NOT NULL,
+    data_iscrizione DATE NOT NULL
 );
 
 CREATE TABLE Admin(
     utente VARCHAR(100) PRIMARY KEY,
     data_termine DATE, -- NULL se ancora admin
-    FOREIGN KEY utente REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Visitatore(
@@ -45,12 +43,12 @@ CREATE TABLE Visitatore(
     biografia VARCHAR(500) NOT NULL,
     tipo_studente CATEGORIA NOT NULL,
     immagine VARCHAR(500) NOT NULL, -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
-    FOREIGN KEY utente REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Ingrediente(
     nome VARCHAR(20) PRIMARY KEY,
-    marca VARCHAR(50),
+    marca VARCHAR(50)
 );
 
 CREATE TABLE Ricetta(
@@ -61,16 +59,15 @@ CREATE TABLE Ricetta(
     admin VARCHAR(100) NOT NULL,
     data DATE NOT NULL,
     immagine VARCHAR(500) NOT NULL, -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
-    --NOTA: UN ADMIN RIMANE NEL DB ANCHE QUANDO LO LICENZIAMO COSÌ RIMANGONO LE SUE RICETTE
-    FOREIGN KEY admin REFERENCES Admin(utente) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (admin) REFERENCES Admin(utente) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
 CREATE TABLE Preferenza_Ricetta(
     ricetta VARCHAR(50) NOT NULL,
     visitatore VARCHAR(100) NOT NULL,
     PRIMARY KEY (ricetta, visitatore),
-    FOREIGN KEY ricetta REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY visitatore REFERENCES Visitatore(utente) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (ricetta) REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (visitatore) REFERENCES Visitatore(utente) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Utilizzo(
@@ -79,20 +76,20 @@ CREATE TABLE Utilizzo(
     quanto_basta BOOLEAN NOT NULL,
     quantita POSITIVE_SMALLINT,
     unita_misura UNITA_MISURA,
-    CHECK(quanto_basta=TRUE AND quantita IS NULL AND unita_misura IS NULL)
-    CHECK(quanto_basta=FALSE AND quantita IS NOT NULL AND unita_misura IS NOT NULL)
+    CHECK(quanto_basta IS TRUE AND quantita IS NULL AND unita_misura IS NULL),
+    CHECK(quanto_basta IS FALSE AND quantita IS NOT NULL AND unita_misura IS NOT NULL),
     descrizione VARCHAR(50),
-    PRIMARY KEY(ingrediente, ricetta)
-    FOREIGN KEY ingrediente REFERENCES Ingrediente(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY ricetta REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY(ingrediente, ricetta),
+    FOREIGN KEY (ingrediente) REFERENCES Ingrediente(nome) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (ricetta) REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Preparazione(
-    numero POSITIVE_TINYINT,
+    numero POSITIVE_SMALLINT,
     ricetta VARCHAR(50),
     descrizione VARCHAR(500) NOT NULL,
     PRIMARY KEY (numero, ricetta),
-    FOREIGN KEY ricetta REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (ricetta) REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Valutazione(
@@ -100,31 +97,30 @@ CREATE TABLE Valutazione(
     visitatore VARCHAR(100),
     commento VARCHAR(500) NOT NULL,
     data DATE NOT NULL,
-    voto POSITIVE_TINYINT NOT NULL,
+    voto POSITIVE_SMALLINT NOT NULL,
     CHECK(voto<=30),
     PRIMARY KEY (ricetta, visitatore),
-    FOREIGN KEY ricetta REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY visitatore REFERENCES Visitatore(utente) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (ricetta) REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (visitatore) REFERENCES Visitatore(utente) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Take_away(
     nome_locale VARCHAR(100) PRIMARY KEY,
     admin VARCHAR(100) NOT NULL,
     indirizzo VARCHAR(30) NOT NULL,
-    civico TINYINT NOT NULL,
+    civico SMALLINT NOT NULL,
     maps VARCHAR(500) NOT NULL,  -- Collegamento web per Maps
     sito VARCHAR(500),
     immagine VARCHAR(500) NOT NULL, -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
     descrizione VARCHAR(500) NOT NULL,
     data DATE NOT NULL,
-    --NOTA: UN ADMIN RIMANE NEL DB ANCHE QUANDO LO LICENZIAMO COSÌ RIMANGONO I SUOI TAKE AWAY
-    FOREIGN KEY admin REFERENCES Admin(utente) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (admin) REFERENCES Admin(utente) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
 CREATE TABLE Preferenza_take_away(
     visitatore VARCHAR(100) NOT NULL,
     take_away VARCHAR(100) NOT NULL,
-    PRIMARY KEY (visitatore, takeaway),
-    FOREIGN KEY takeaway REFERENCES Takeaway(nome_locale) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY visitatore REFERENCES Visitatore(utente) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (visitatore, take_away),
+    FOREIGN KEY (take_away) REFERENCES Take_away(nome_locale) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (visitatore) REFERENCES Visitatore(utente) ON DELETE CASCADE ON UPDATE CASCADE
 );
