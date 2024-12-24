043301f1a03702eaec611db90452e8a63f2bde9f
aprecoma <andreaprecoma20@gmail.com>
2024-12-24 22:37:55 +0100

    Sistemato stile orribile del PHP e cosucce sui html

--- test/PHP/UtilityMethods.php
+++ test/PHP/UtilityMethods.php
@@ -1,136 +1,105 @@
 <?php
-    namespace DB;
 
-    session_start();
+namespace DB;
 
-    class DB
-    {
-        private $tagPermessi ='<em><strong><ul><li>';
-        private const HOST_DB = "localhost";
-        private const DBNAME = "placeholder";
-        private const USERNAME = "placeholder";
-        private const PSW = "placeholder";
+session_start();
+
+class DB {
+    private $tagPermessi ='<em><strong><ul><li>';
+    private const HOST_DB = "localhost";
+    private const DBNAME = "placeholder";
+    private const USERNAME = "placeholder";
+    private const PSW = "placeholder";
+
+    private $connection;
+
+    public static function pulisciInput($value) {
+        $value = trim($value);
+        $value = strip_tags($value);
+        $value = htmlentities($value);
+        return $value;
+    }
     
-        private $connection;
-
-        public static function pulisciInput($value)
-        {
-            $value = trim($value);
-            $value = strip_tags($value);
-            $value = htmlentities($value);
-            return $value;
-        }
-       
-        public static function pulisciNote($value)
-        {
-            global $tagPermessi;
-            $value = trim($value);
-            $value = strip_tags($value, $tagPermessi);
-            return $value;
-        }
-
-        public function openDBConnection(): bool
-        {
-            mysqli_report(MYSQLI_REPORT_STRICT);
-            try
-            {
-                $this->connection = mysqli_connect(self::HOST_DB, self::USERNAME, self::PSW, self::DBNAME);
-            } 
-			catch(\mysqli_sql_exception $e)
-			{
-				return false;
-            }
-
-			return true;
-        }
-        
-        public function closeDBConnection(): void
-        {
-            mysqli_close($this->connection);
-        }
-
-        public function checkUserPresence($username): bool | string
-        {
-            $connectionResult=$this->openDBConnection();
-            if($connectionResult)
-            {
-                $checkUserStatement=$this->connection->prepare("SELECT nome FROM Utente WHERE nome = ?");
-                $checkUserStatement->bind_param("s",$username);
-                try
-                {
-                    $checkUserStatement->execute();
-                }
-                catch(\mysqli_sql_exception $e)
-                {
-                    $this->closeDBConnection();
-                    $checkUserStatement->close();
-                    return "ExceptionThrow";
-                }
-
-                $result=$checkUserStatement->get_result();
-                $this->closeDBConnection();
-                $checkUserStatement->close();
-
-                if($result->num_rows==1)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-                
-            }
-            else
-            {
-                return "ConnectionFailed";
-            }
-        }
-
-        public function registerUser($username,$userCategory,$email,$psw): bool
-        {   
-            /*
-                Gestisce l'inserimento di dati già appositamente filtrati. Ritorna false se l'inserimento fallisce (es. l'utente è già registrato o la connessione
-                al server è fallita)
-            */
-            $encPsw=hash('sha256',$psw);
-            $date=date("Y-m-d h:m:s");
-            $connectionResult=$this->openDBConnection();
-            if($connectionResult)
-            {   
-                $registerUserStatement=$this->connection->prepare("INSERT INTO Utente(email,nome,password,data_iscrizione) VALUES(?,?,?,?)");
-                $registerUserStatement->bind_param("ssss",$email,$username,$encPsw,$date);
-                try
-                {
-                    $registerUserStatement->execute();
-                }
-                catch(\mysqli_sql_exception $e)
-                {
-                    $this->closeDBConnection();
-                    $registerUserStatement->close();
-                    return false;
-                }
-
-                if(mysqli_affected_rows($this->connection)==1)
-                {
-                    $this->closeDBConnection();
-                    $registerUserStatement->close();
-                    $_SESSION["logged_user"] = $username; 
-                    return true;
-                }
-                else
-                {
-                    $registerUserStatement->close();
-                    $this->closeDBConnection();
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
-
+    public static function pulisciNote($value) {
+        global $tagPermessi;
+        $value = trim($value);
+        $value = strip_tags($value, $tagPermessi);
+        return $value;
     }
 
-?>
+    public function openDBConnection(): bool {
+        mysqli_report(MYSQLI_REPORT_STRICT);
+        try {
+            $this->connection = mysqli_connect(self::HOST_DB, self::USERNAME, self::PSW, self::DBNAME);
+        } catch(\mysqli_sql_exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+    
+    public function closeDBConnection(): void {
+        mysqli_close($this->connection);
+    }
+
+    public function checkUserPresence($username): bool | string {
+        $connectionResult=$this->openDBConnection();
+        if ($connectionResult) {
+            $checkUserStatement=$this->connection->prepare("SELECT nome FROM Utente WHERE nome = ?");
+            $checkUserStatement->bind_param("s",$username);
+            try {
+                $checkUserStatement->execute();
+            } catch(\mysqli_sql_exception $e) {
+                $this->closeDBConnection();
+                $checkUserStatement->close();
+                return "ExceptionThrow";
+            }
+
+            $result=$checkUserStatement->get_result();
+            $this->closeDBConnection();
+            $checkUserStatement->close();
+
+            if ($result->num_rows==1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return "ConnectionFailed";
+        }
+    }
+
+    public function registerUser($username,$userCategory,$email,$psw): bool {
+        /*
+            Gestisce l'inserimento di dati già appositamente filtrati. Ritorna false se l'inserimento fallisce (es. l'utente è già registrato o la connessione
+            al server è fallita)
+        */
+        $encPsw=hash('sha256',$psw);
+        $date=date("Y-m-d h:m:s");
+        $connectionResult=$this->openDBConnection();
+        if ($connectionResult) {
+            $registerUserStatement=$this->connection->prepare("INSERT INTO Utente(email,nome,password,data_iscrizione) VALUES(?,?,?,?)");
+            $registerUserStatement->bind_param("ssss",$email,$username,$encPsw,$date);
+            try {
+                $registerUserStatement->execute();
+            } catch(\mysqli_sql_exception $e) {
+                $this->closeDBConnection();
+                $registerUserStatement->close();
+                return false;
+            }
+
+            if (mysqli_affected_rows($this->connection)==1) {
+                $this->closeDBConnection();
+                $registerUserStatement->close();
+                $_SESSION["logged_user"] = $username; 
+                return true;
+            } else {
+                $registerUserStatement->close();
+                $this->closeDBConnection();
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
--- test/PHP/sign-up.php
+++ test/PHP/sign-up.php
@@ -1,116 +1,96 @@
 <?php
 
-    require_once "UtilityMethods.php";
-    use DB\DB;
+require_once "utilityMethods.php";
+use DB\DB;
 
-    $username="";
-    $categoria="";
-    $mail="";
-    $psw="";
-    $db = new DB;
+$username="";
+$categoria="";
+$mail="";
+$psw="";
+$db = new DB;
 
-    $paginaHtml=file_get_contents("html/sign-up.html");
-    if(isset($_POST['submit'])) {
-        $errorFound=false;
-        $username=$_POST["nome"];
-        //CONTROLLO USERNAME
-        if(strlen($username)==0) {
-            $paginaHtml = str_replace("{{messaggio di nome}}","Il nome utente è un campo obbligatorio.",$paginaHtml);
+$paginaHtml=file_get_contents("html/sign-up.html");
+if (isset($_POST['submit'])) {
+    $errorFound=false;
+    $username=$_POST["nome"];
+    //CONTROLLO USERNAME
+    if (strlen($username)==0) {
+        $paginaHtml = str_replace("{{messaggio di nome}}","Il nome utente è un campo obbligatorio.",$paginaHtml);
+        $errorFound=true;
+    } else {
+        $username=DB::pulisciInput($username);
+        $isUserPresent=$db->checkUserPresence($username);
+        if (strcmp($isUserPresent,"ExceptionThrow")!=0 && strcmp($isUserPresent,"ConnectionFailed")!=0 && $isUserPresent==true) {
             $errorFound=true;
-        }
-        else {
-            $username=DB::pulisciInput($username);
-            $isUserPresent=$db->checkUserPresence($username);
-            if(strcmp($isUserPresent,"ExceptionThrow")!=0 && strcmp($isUserPresent,"ConnectionFailed")!=0 && $isUserPresent==true) {
-                $errorFound=true;
-                $paginaHtml = str_replace("{{messaggio di nome}}","Questo nome utente non può essere utilizzato.",$paginaHtml);
-            }
-            elseif (strcmp($isUserPresent,"ExceptionThrow")==0 || strcmp($isUserPresent,"ConnectionFailed")==0){
-                $_POST = null;
-                header('Location: 505-err.php');
-                exit();
-            }
-            else
-            {
-                $paginaHtml = str_replace("{{messaggio di nome}}","",$paginaHtml);
-            }
-        }
-
-        //CONTROLLO CATEGORIA
-        if (!isset($_POST['categoria'])){
-            $errorFound=true;
-            $paginaHtml = str_replace("{{messaggio di categoria}}","Seleziona una categoria.",$paginaHtml);
-        }
-        elseif(strcmp($_POST['categoria'],"fuorisede")==0 || strcmp($_POST['categoria'],"pendolare")==0 || strcmp($_POST['categoria'],"in-sede")==0|| strcmp($_POST['categoria'],"dad")==0){
-            $paginaHtml = str_replace("{{messaggio di categoria}}","",$paginaHtml);
-            $categoria=$_POST['categoria'];
-        }
-        else{
-            $errorFound=true;
-            $paginaHtml = str_replace("{{messaggio di categoria}}","La categoria inserita non è valida.",$paginaHtml);
-        }
-
-        //CONTROLLO EMAIL
-        $email=$_POST['email'];
-        if(strlen($email)==0){
-            $errorFound=true;
-            $paginaHtml = str_replace("{{messaggio di email}}","Il campo <span lang=\"en\">email</span> è un campo obbligatorio.",$paginaHtml);
-        }
-        else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            $paginaHtml = str_replace("{{messaggio di email}}","L'<span lang=\"en\">email</span> non è un indirizzo valido.",$paginaHtml);
-        }
-        else
-        {
-            $paginaHtml = str_replace("{{messaggio di email}}","",$paginaHtml);
-        }
-
-        //CONTROLLO PSW
-        $psw=$_POST['psw'];
-        if(strlen($psw)==0)
-        {
-            $errorFound=true;
-            $paginaHtml = str_replace("{{messaggio di psw-ripetuta}}","",$paginaHtml);
-            $paginaHtml = str_replace("{{messaggio di psw}}","Il campo <span lang=\"en\">password</span> è obbligatorio.",$paginaHtml);
-        }
-        elseif(strlen($psw)<4)
-        {
-            $errorFound=true;
-            $paginaHtml = str_replace("{{messaggio di psw-ripetuta}}","",$paginaHtml);
-            $paginaHtml = str_replace("{{messaggio di psw}}","La <span lang=\"en\">password</span> deve essere lunga almeno 4 caratteri",$paginaHtml);
-        }
-        elseif(strcmp($psw,$_POST['repeat-psw'])!=0)
-        {
-            $paginaHtml = str_replace("{{messaggio di psw}}","",$paginaHtml);
-            $errorFound=true;
-            $paginaHtml = str_replace("{{messaggio di psw-ripetuta}}","La <span lang=\"en\">password</span> non coincide.",$paginaHtml);
-        }
-        else
-        {
-            $paginaHtml = str_replace("{{messaggio di psw}}","",$paginaHtml);
-            $paginaHtml = str_replace("{{messaggio di psw-ripetuta}}","",$paginaHtml);
-        }
-
-        if($errorFound==false){
-            if(strcmp($categoria,"in-sede"))
-            {
-                $categoria="in_sede"; //fix per enum db
-            }
-            $db->registerUser($username,$categoria,$email,$psw);
+            $paginaHtml = str_replace("{{messaggio di nome}}","Questo nome utente non può essere utilizzato.",$paginaHtml);
+        } elseif (strcmp($isUserPresent,"ExceptionThrow")==0 || strcmp($isUserPresent,"ConnectionFailed")==0) {
             $_POST = null;
-            header('Location: index.php');
+            header('Location: 505-err.php');
             exit();
-        }
-        else
-        {
-            echo $paginaHtml;
+        } else {
+            $paginaHtml = str_replace("{{messaggio di nome}}","",$paginaHtml);
         }
     }
-    else {
-        $paginaHtml = str_replace("{{messaggio di nome}}","",$paginaHtml);
+
+    //CONTROLLO CATEGORIA
+    if (!isset($_POST['categoria'])) {
+        $errorFound=true;
+        $paginaHtml = str_replace("{{messaggio di categoria}}","Seleziona una categoria.",$paginaHtml);
+    } elseif (strcmp($_POST['categoria'],"fuorisede")==0 || strcmp($_POST['categoria'],"pendolare")==0 || strcmp($_POST['categoria'],"in-sede")==0|| strcmp($_POST['categoria'],"dad")==0) {
         $paginaHtml = str_replace("{{messaggio di categoria}}","",$paginaHtml);
+        $categoria=$_POST['categoria'];
+    } else {
+        $errorFound=true;
+        $paginaHtml = str_replace("{{messaggio di categoria}}","La categoria inserita non è valida.",$paginaHtml);
+    }
+
+    //CONTROLLO EMAIL
+    $email=$_POST['email'];
+    if (strlen($email)==0) {
+        $errorFound=true;
+        $paginaHtml = str_replace("{{messaggio di email}}","Il campo <span lang=\"en\">email</span> è un campo obbligatorio.",$paginaHtml);
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $paginaHtml = str_replace("{{messaggio di email}}","L'<span lang=\"en\">email</span> non è un indirizzo valido.",$paginaHtml);
+    } else {
         $paginaHtml = str_replace("{{messaggio di email}}","",$paginaHtml);
+    }
+
+    //CONTROLLO PSW
+    $psw=$_POST['psw'];
+    if (strlen($psw)==0) {
+        $errorFound=true;
+        $paginaHtml = str_replace("{{messaggio di psw-ripetuta}}","",$paginaHtml);
+        $paginaHtml = str_replace("{{messaggio di psw}}","Il campo <span lang=\"en\">password</span> è obbligatorio.",$paginaHtml);
+    } elseif (strlen($psw)<4) {
+        $errorFound=true;
+        $paginaHtml = str_replace("{{messaggio di psw-ripetuta}}","",$paginaHtml);
+        $paginaHtml = str_replace("{{messaggio di psw}}","La <span lang=\"en\">password</span> deve essere lunga almeno 4 caratteri",$paginaHtml);
+    } elseif (strcmp($psw,$_POST['repeat-psw'])!=0) {
+        $paginaHtml = str_replace("{{messaggio di psw}}","",$paginaHtml);
+        $errorFound=true;
+        $paginaHtml = str_replace("{{messaggio di psw-ripetuta}}","La <span lang=\"en\">password</span> non coincide.",$paginaHtml);
+    } else {
         $paginaHtml = str_replace("{{messaggio di psw}}","",$paginaHtml);
         $paginaHtml = str_replace("{{messaggio di psw-ripetuta}}","",$paginaHtml);
+    }
+
+    if ($errorFound==false) {
+        if (strcmp($categoria,"in-sede")) {
+            $categoria="in_sede"; //fix per enum db
+        }
+        $db->registerUser($username,$categoria,$email,$psw);
+        $_POST = null;
+        header('Location: index.php');
+        exit();
+    } else {
         echo $paginaHtml;
     }
-?>
+} else {
+    $paginaHtml = str_replace("{{messaggio di nome}}","",$paginaHtml);
+    $paginaHtml = str_replace("{{messaggio di categoria}}","",$paginaHtml);
+    $paginaHtml = str_replace("{{messaggio di email}}","",$paginaHtml);
+    $paginaHtml = str_replace("{{messaggio di psw}}","",$paginaHtml);
+    $paginaHtml = str_replace("{{messaggio di psw-ripetuta}}","",$paginaHtml);
+    echo $paginaHtml;
+}
