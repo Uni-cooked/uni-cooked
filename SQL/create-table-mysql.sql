2f96121a9ab1f56f2a29ce390c325ce8cbce7975
aprecoma <andreaprecoma20@gmail.com>
2024-12-24 16:21:15 +0100

    Sistemato create table

--- SQL/create-table-mysql.sql
+++ SQL/create-table-mysql.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS Utente;
-DROP TABLE IF EXISTS Admin;
-DROP TABLE IF EXISTS Vistatore;
+-- DROP TABLE IF EXISTS Admin;
+-- DROP TABLE IF EXISTS Vistatore;
 DROP TABLE IF EXISTS Ingrediente;
 DROP TABLE IF EXISTS Ricetta;
 DROP TABLE IF EXISTS Preferenza_Ricetta;
@@ -12,22 +12,25 @@ CREATE TABLE Utente(
     email VARCHAR(100) PRIMARY KEY,
     nome VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(64) NOT NULL,  -- SHA256 restituisce sempre una stringa di 64 caratteri
-    data_iscrizione DATE NOT NULL
-);
-
-CREATE TABLE Admin(
-    utente VARCHAR(100) PRIMARY KEY,
-    data_termine DATE, -- NULL se ancora admin
-    FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE Visitatore(
-    utente VARCHAR(100) PRIMARY KEY,
+    data_iscrizione DATE NOT NULL,
     biografia VARCHAR(500) NOT NULL,
-    tipo_studente enum ('fuorisede', 'in_sede', 'pendolare', 'dad', 'take_away') NOT NULL,
-    immagine VARCHAR(500) NOT NULL, -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
-    FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
+    tipo_studente enum ('fuorisede', 'pendolare', 'in_sede', 'dad') NOT NULL,
+    immagine VARCHAR(500) NOT NULL -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
 );
+
+-- CREATE TABLE Admin(
+--     utente VARCHAR(100) PRIMARY KEY,
+--     data_termine DATE, -- NULL se ancora admin
+--     FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
+-- );
+
+-- CREATE TABLE Visitatore(
+--     utente VARCHAR(100) PRIMARY KEY,
+--     biografia VARCHAR(500) NOT NULL,
+--     tipo_studente enum ('fuorisede', 'pendolare', 'in_sede', 'dad') NOT NULL,
+--     immagine VARCHAR(500) NOT NULL, -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
+--     FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
+-- );
 
 CREATE TABLE Ingrediente(
     nome VARCHAR(20) PRIMARY KEY
@@ -35,29 +38,29 @@ CREATE TABLE Ingrediente(
 
 CREATE TABLE Ricetta(
     nome VARCHAR(50) PRIMARY KEY,
-    categoria enum ('fuorisede', 'in_sede', 'pendolare', 'dad', 'take_away') NOT NULL,
+    categoria enum ('fuorisede', 'pendolare', 'in_sede', 'dad') NOT NULL,
     tipo enum ('primo', 'secondo') NOT NULL,
-    tempo INT NOT NULL CHECK (tempo>=0),
+    tempo INT NOT NULL,   -- tempo in secondi
     descrizione VARCHAR(500) NOT NULL,
-    admin VARCHAR(100) NOT NULL,
+    -- admin VARCHAR(100) NOT NULL,
     data DATE NOT NULL,
-    immagine VARCHAR(500) NOT NULL, -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
-    FOREIGN KEY (admin) REFERENCES Admin(utente) ON DELETE NO ACTION ON UPDATE CASCADE
+    immagine VARCHAR(500) NOT NULL -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
+    -- FOREIGN KEY (admin) REFERENCES Admin(utente) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
 CREATE TABLE Preferenza_Ricetta(
     ricetta VARCHAR(50) NOT NULL,
-    visitatore VARCHAR(100) NOT NULL,
-    PRIMARY KEY (ricetta, visitatore),
+    utente VARCHAR(100) NOT NULL,
+    PRIMARY KEY (ricetta, utente),
     FOREIGN KEY (ricetta) REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (visitatore) REFERENCES Visitatore(utente) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Utilizzo(
     ingrediente VARCHAR(20) NOT NULL,
     ricetta VARCHAR(50) NOT NULL,
     quanto_basta BOOLEAN NOT NULL,
-    quantita INT CHECK(quantita>=0),
+    quantita SMALLINT CHECK(quantita>=0),
     unita_misura enum ('g', 'ml', 'num_elementi'),
     CHECK(quanto_basta IS TRUE AND quantita IS NULL AND unita_misura IS NULL),
     CHECK(quanto_basta IS FALSE AND quantita IS NOT NULL AND unita_misura IS NOT NULL),
@@ -68,7 +71,7 @@ CREATE TABLE Utilizzo(
 );
 
 CREATE TABLE Preparazione(
-    numero INT,
+    numero TINYINT,
     ricetta VARCHAR(50),
     descrizione VARCHAR(500) NOT NULL,
     PRIMARY KEY (numero, ricetta),
@@ -77,12 +80,11 @@ CREATE TABLE Preparazione(
 
 CREATE TABLE Valutazione(
     ricetta VARCHAR(50),
-    visitatore VARCHAR(100),
+    utente VARCHAR(100),
     commento VARCHAR(500) NOT NULL,
     data DATE NOT NULL,
-    voto INT NOT NULL,
-    CHECK(voto>0 AND voto<=30),
-    PRIMARY KEY (ricetta, visitatore),
+    voto TINYINT NOT NULL CHECK(voto>0 AND voto<=30),
+    PRIMARY KEY (ricetta, utente),
     FOREIGN KEY (ricetta) REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (visitatore) REFERENCES Visitatore(utente) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
 );
--- SQL/create-table.sql
+++ SQL/create-table.sql
@@ -1,78 +1,49 @@
-DROP TABLE IF EXISTS Utente CASCADE;
-DROP TABLE IF EXISTS Admin CASCADE;
-DROP TABLE IF EXISTS Vistatore CASCADE;
-DROP TABLE IF EXISTS Ingrediente CASCADE;
-DROP TABLE IF EXISTS Ricetta CASCADE;
-DROP TABLE IF EXISTS Preferenza_Ricetta CASCADE;
-DROP TABLE IF EXISTS Utilizzo CASCADE;
-DROP TABLE IF EXISTS Preparazione CASCADE;
-DROP TABLE IF EXISTS Valutazione CASCADE;
-DROP TABLE IF EXISTS Take_away CASCADE;
-DROP TABLE IF EXISTS Preferenza_take_away CASCADE;
-
-DROP DOMAIN IF EXISTS POSITIVE_SMALLINT;
-
-DROP TYPE IF EXISTS CATEGORIA;
-DROP TYPE IF EXISTS TIPO_PIATTO;
-DROP TYPE IF EXISTS UNITA_MISURA;
-
-CREATE DOMAIN POSITIVE_SMALLINT AS SMALLINT CHECK (VALUE>=0);   -- 2 Byte
-
-CREATE TYPE CATEGORIA AS enum ('fuorisede', 'in_sede', 'pendolare', 'dad', 'take_away');
-CREATE TYPE TIPO_PIATTO AS enum ('primo', 'secondo');
-CREATE TYPE UNITA_MISURA AS enum ('g', 'ml', 'num_elementi');
+DROP TABLE IF EXISTS Preferenza_Ricetta;
+DROP TABLE IF EXISTS Valutazione;
+DROP TABLE IF EXISTS Preparazione;
+DROP TABLE IF EXISTS Utilizzo_Ingrediente;
+DROP TABLE IF EXISTS Ricetta;
+DROP TABLE IF EXISTS Utente;
+DROP TABLE IF EXISTS Ingrediente;
 
 CREATE TABLE Utente(
     email VARCHAR(100) PRIMARY KEY,
     nome VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(64) NOT NULL,  -- SHA256 restituisce sempre una stringa di 64 caratteri
-    data_iscrizione DATE NOT NULL
-);
-
-CREATE TABLE Admin(
-    utente VARCHAR(100) PRIMARY KEY,
-    data_termine DATE, -- NULL se ancora admin
-    FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE Visitatore(
-    utente VARCHAR(100) PRIMARY KEY,
+    data_iscrizione DATE NOT NULL,
     biografia VARCHAR(500) NOT NULL,
-    tipo_studente CATEGORIA NOT NULL,
-    immagine VARCHAR(500) NOT NULL, -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
-    FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
+    tipo_studente enum ('fuorisede', 'pendolare', 'in_sede', 'dad') NOT NULL,
+    immagine VARCHAR(500) NOT NULL -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
 );
 
 CREATE TABLE Ingrediente(
-    nome VARCHAR(20) PRIMARY KEY,
+    nome VARCHAR(20) PRIMARY KEY
 );
 
 CREATE TABLE Ricetta(
     nome VARCHAR(50) PRIMARY KEY,
-    categoria CATEGORIA NOT NULL,
-    tipo TIPO_PIATTO NOT NULL,
-    tempo POSITIVE_SMALLINT NOT NULL,
+    categoria enum ('fuorisede', 'pendolare', 'in_sede', 'dad') NOT NULL,
+    tipo_piatto enum ('primo', 'secondo') NOT NULL,
+    tempo_sec INT NOT NULL,
     descrizione VARCHAR(500) NOT NULL,
-    admin VARCHAR(100) NOT NULL,
     data DATE NOT NULL,
-    immagine VARCHAR(500) NOT NULL, -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
-    FOREIGN KEY (admin) REFERENCES Admin(utente) ON DELETE NO ACTION ON UPDATE CASCADE
+    immagine VARCHAR(500) NOT NULL -- Ancora non sappiamo il tipo, per ora faccio finta sia una directory
 );
 
 CREATE TABLE Preferenza_Ricetta(
     ricetta VARCHAR(50) NOT NULL,
-    visitatore VARCHAR(100) NOT NULL,
-    PRIMARY KEY (ricetta, visitatore),
+    utente VARCHAR(100) NOT NULL,
+    PRIMARY KEY (ricetta, utente),
     FOREIGN KEY (ricetta) REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (visitatore) REFERENCES Visitatore(utente) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Utilizzo(
+CREATE TABLE Utilizzo_Ingrediente(
     ingrediente VARCHAR(20) NOT NULL,
     ricetta VARCHAR(50) NOT NULL,
     quanto_basta BOOLEAN NOT NULL,
-    quantita POSITIVE_SMALLINT,
-    unita_misura UNITA_MISURA,
+    quantita SMALLINT CHECK(quantita>=0),
+    unita_misura enum ('g', 'ml', 'num_elementi'),
     CHECK(quanto_basta IS TRUE AND quantita IS NULL AND unita_misura IS NULL),
     CHECK(quanto_basta IS FALSE AND quantita IS NOT NULL AND unita_misura IS NOT NULL),
     descrizione VARCHAR(50),
@@ -91,12 +62,11 @@ CREATE TABLE Preparazione(
 
 CREATE TABLE Valutazione(
     ricetta VARCHAR(50),
-    visitatore VARCHAR(100),
+    utente VARCHAR(100),
     commento VARCHAR(500) NOT NULL,
     data DATE NOT NULL,
-    voto TINYINT NOT NULL,
-    CHECK(voto>0 AND voto<=30),
-    PRIMARY KEY (ricetta, visitatore),
+    voto TINYINT NOT NULL CHECK(voto>0 AND voto<=30),
+    PRIMARY KEY (ricetta, utente),
     FOREIGN KEY (ricetta) REFERENCES Ricetta(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (visitatore) REFERENCES Visitatore(utente) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (utente) REFERENCES Utente(email) ON DELETE CASCADE ON UPDATE CASCADE
 );
