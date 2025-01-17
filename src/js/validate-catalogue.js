fe97a5bcb11fbe75528151b0a3f4a40968f0bf5e
GabryMagno <153952005+GabryMagno@users.noreply.github.com>
2025-01-17 23:49:05 +0100

    Update validate-catalogue.js

--- src/js/validate-catalogue.js
+++ src/js/validate-catalogue.js
@@ -1,15 +1,23 @@
 function FilterForm(){
     let form = document.getElementById("search-fields");
-    let vote = document.getElementById("min-rate-filter").value;
-    let cost =  document.getElementById("max-price-filter").value;
+    var vote = Calculation(localStorage.getItem(vote),document.getElementById("min-rate-filter").value);
+    var cost =  Calculation(localStorage.getItem(cost),document.getElementById("max-price-filter").value);
     form.addEventListener("submit", () => { 
         localStorage.setItem(vote , document.getElementById("min-rate-filter").value);
         localStorage.setItem(cost , document.getElementById("max-price-filter").value);
     });
-    document.getElementById("min-rate-filter").value = localStorage.getItem(vote); 
-    document.getElementById("max-price-filter").value = localStorage.getItem(cost);
-    document.getElementById("rate").innerText = localStorage.getItem(vote) + " / 30";
-    document.getElementById("price").innerText = localStorage.getItem(cost) + " €";
+    if(localStorage.getItem(vote) >= 1 && localStorage.getItem(cost) >= 1){
+        document.getElementById("min-rate-filter").value = localStorage.getItem(vote);
+        document.getElementById("max-price-filter").value = localStorage.getItem(cost);
+        document.getElementById("rate").innerText = localStorage.getItem(vote) + " / 30";
+        document.getElementById("price").innerText = localStorage.getItem(cost) + " €";
+        localStorage.clear();
+    }
+}
+
+function Calculation(x,y){
+    if(x) return x;
+    else return y;
 }
 
 function UpdateRangeVoto(){
