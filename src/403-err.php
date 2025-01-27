491ea6e309111ea27163c6e5a1648d3ca4c8ddd6
aprecoma <andreaprecoma20@gmail.com>
2025-01-27 15:57:07 +0100

    aggiunta 403.php

---
+++ src/403-err.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "utils/utility-methods.php";
+use Utilities\DB;
+$db = new DB;
+
+$paginaHtml=file_get_contents("./html/403-err.html");
+if($db->isUserLogged()!=false) {
+    echo str_replace("{{link-accesso-profilo}}","<a href=\"user.php\" class=\"a-btn-secondary shadow\">PROFILO</a>",$paginaHtml);
+} else {
+    echo str_replace("{{link-accesso-profilo}}","<a href=\"sign-in.php\" class=\"a-btn-secondary shadow\">ACCEDI</a>",$paginaHtml); 
+}
